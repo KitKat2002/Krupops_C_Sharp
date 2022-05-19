cd295457cb4546a6bca8e1a9d3d347725eb0d7e6
KitKat2002 <89272992+KitKat2002@users.noreply.github.com>
2022-05-19 00:31:51 -0600

    commit

---
+++ InsuranceProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InsuranceProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?  Please answer in true or false.");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets haave you received?");
+            int speedTickets = Convert.ToInt32(Console.ReadLine());
+
+            bool applicable = (age >= 15 && DUI == false && (speedTickets == 0));
+            Console.WriteLine("Find your results below");
+            Console.WriteLine(applicable);
+        }
+    }
+}
