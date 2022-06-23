603eed876d5f102b7f1cb9cf8e2007dcf5032fca
KitKat2002 <89272992+KitKat2002@users.noreply.github.com>
2022-06-23 01:15:25 -0600

    Commit

---
+++ Methods/MathOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+        public class MathClass 
+        {
+            public int Add (int MathNum_A, int MathNum_B)
+            {
+                int result = MathNum_A + MathNum_B;
+                return result;
+            }
+            public int Sub(int MathNum_A, int MathNum_B)
+            {
+                int result = MathNum_A - MathNum_B;
+                return result;
+            }
+            public int Rem(int MathNum_A, int MathNum_B)
+            {
+                int result = MathNum_A % MathNum_B;
+                return result;
+            }
+        }
+    }
---
+++ Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to have it added to, subracted from, and the remainder found by three different pre-programmed numbers.");
+            int MathNum_A = Convert.ToInt32(Console.ReadLine());
+            int MathNum_B = 7;
+            int result;
+            MathClass newFunction = new MathClass();
+
+            Console.WriteLine("Addition");
+            result = newFunction.Add(MathNum_A, MathNum_B);
+            Console.WriteLine(result);
+
+            Console.WriteLine("Subtraction");
+            result = newFunction.Sub(MathNum_A, MathNum_B);
+            Console.WriteLine(result);
+
+            Console.WriteLine("Remainder");
+            result = newFunction.Rem(MathNum_A, MathNum_B);
+            Console.WriteLine(result);
+
+        }
+    }
+}
