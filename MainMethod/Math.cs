153ae3d7ff142dd7ef4ea3e839796b2f1d18a23d
KitKat2002 <89272992+KitKat2002@users.noreply.github.com>
2022-06-25 22:43:34 -0600

    commit

---
+++ MainMethod/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMethod
+{
+   public class Math
+    {
+        public int problem(int DivNum_A)
+        {
+            int DivNum_B = 14;
+            int answer = DivNum_A / DivNum_B;
+            return answer;
+        }
+        public decimal problem(decimal RemNum_A)
+        {
+            decimal RemNum_B = 3.75m;
+            decimal answer2 = RemNum_A % RemNum_B;
+            return answer2;
+        }
+        public int problem(string Conversion)
+        {
+            int ConNum_A = 4;
+            int ConNum_B = Convert.ToInt32(Conversion);
+            int answer3 = ConNum_A - ConNum_B;
+            return answer3;
+        }
+    }
+}
---
+++ MainMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int DivNum_A = 14;
+            Math divisionFunction = new Math();
+            Console.WriteLine("Division");
+            int answer = divisionFunction.problem(DivNum_A);
+            Console.WriteLine(answer);
+
+            decimal RemNum_A = 2.62m;
+            Math remainderFunction = new Math();
+            Console.WriteLine("Remainder of decimal");
+            decimal answer2 = remainderFunction.problem(RemNum_A);
+            Console.WriteLine(answer2);
+
+            Math stringFunction = new Math();
+            Console.WriteLine("Enter a number");
+            string Conversion = Console.ReadLine();
+            Console.WriteLine("Conversion of string to int and had had four subtracted from");
+            int answer3 = stringFunction.problem(Conversion);
+            Console.WriteLine(answer3);
+            
+
+        }
+    }
+}
