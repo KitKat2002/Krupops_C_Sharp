132e88e6ab9ae51787fd97871013fb53584c2a30
KitKat2002 <89272992+KitKat2002@users.noreply.github.com>
2022-06-27 01:47:29 -0600

    commit

---
+++ MethodAs2/Math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodAs2
+{
+  public class Math
+    {
+        public int problem(int numeroUno, int numeroDos)
+        {
+            int answer = numeroUno + numeroDos;
+            return answer;
+        }
+    }
+}
---
+++ MethodAs2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MethodAs2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math problemFunction = new Math();
+            Console.WriteLine("Please enter a number");
+            int numeroUno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second optional number");
+            string stringNumber = Console.ReadLine();
+            if (string.IsNullOrEmpty(stringNumber))
+            {
+                stringNumber = "4";
+                int numeroDos = Convert.ToInt32(stringNumber);
+
+                int answer = problemFunction.problem(numeroUno, numeroDos);
+                Console.WriteLine(numeroUno + numeroDos);
+                Console.WriteLine(answer);
+            } else
+            {
+                numeroUno = Convert.ToInt32(stringNumber);
+                int numeroDos = Convert.ToInt32(stringNumber);
+                int answer = problemFunction.problem(numeroUno, numeroDos);
+                Console.WriteLine(numeroUno + numeroDos);
+                Console.WriteLine(answer);
+
+                
+            }
+        }
+    }
+}
